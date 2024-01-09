54c31ac2d2b373ed28653838f4e4a64dbf7d2cb6
Masahiro Hayashi <mhayashi1120@gmail.com>
2024-01-09 11:04:46 +0900

    version up

--- package.scm
+++ package.scm
@@ -4,7 +4,7 @@
 
 (define-gauche-package "Gauche-rfc-jwt"
   ;;
-  :version "0.2.0-alpha"
+  :version "0.2.0-alpha2"
 
   ;; Description of the package.  The first line is used as a short
   ;; summary.
